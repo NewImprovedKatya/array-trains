b33677972f1216be3f58fdef45518f227d3ef6db
Katya Greimes <katyagreimes@gmail.com>
2024-04-28 17:14:26 -0700

    Refactored

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let myTracks = [];
 let level = 0;
 let points = 0;
@@ -32,19 +31,42 @@ function play() {
 }
 
 submitButton.addEventListener("click", submitAnswer);
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    submitButton.click();
+  }
+});
 
 function submitAnswer() {
-  event.preventDefault(); //Research this later
+  const removeSpacesAndSemicolons = /[\s;]/g;
   let userInput = document.getElementById("input").value;
-  usurInput = userInput.replace(/smartquoteChars/g, "'");
 
-  if (userInput === levelInfo[level].answer) {
+  let middleInput = userInput.replace(/["\u2018\u2019]/g, "'");
+  let finalInput = middleInput.replace(removeSpacesAndSemicolons, "");
+
+  const finalAnswer = levelInfo[level].answer.replace(
+    removeSpacesAndSemicolons,
+    ""
+  );
+
+
+  if (finalInput === finalAnswer) {
     message.textContent = "Success!";
     nextLevelButton.disabled = false;
 
     points += 10;
     submitButton.disabled = true;
+    refreshTrain();
     levelInfo[level].action();
+    console.log(myTracks);
+
+     for (const car of myTracks) {
+      const trainCar = document.createElement("img");
+      trainCar.src = `img/${car}.svg`;
+      trainCar.className = "slot";
+      trainWrapper.appendChild(trainCar);
+    } 
 
     trainArray.textContent = `const myTracks = [${myTracks.join(", ")}];`;
   } else {
@@ -53,6 +75,12 @@ function submitAnswer() {
   }
 
   pointsDisplay.textContent = "Points: " + points;
+}
+
+function refreshTrain() {
+  while(trainWrapper.firstChild) {
+      trainWrapper.removeChild(trainWrapper.firstChild);
+  }
 }
 
 hintButton.addEventListener("click", revealHint);
@@ -67,7 +95,8 @@ nextLevelButton.addEventListener("click", nextLevel);
 
 function nextLevel() {
   if (level === totalLevels - 1) {
-    message.textContent = "Congratulations! You've completed every available level.";
+    message.textContent =
+      "Congratulations! You've completed every available level.";
     challenge.textContent = `Your total score is ${points} out of ${
       totalLevels * 10
     }!`;
@@ -95,7 +124,7 @@ function nextLevel() {
 const levelInfo = [
   {
     hint: false,
-    car: 'track',
+    car: "track",
     info: `In JavaScript, an array is a special type of object that contains a collection of items. Array items are always aligned within square brackets, kind of like how train cars are aligned on a track.`,
     challenge: `Type the following code to establish your empty array and lay down the tracks:`,
     answer: `const myTracks = [];`,
@@ -107,58 +136,49 @@ const levelInfo = [
   },
   {
     hint: false,
-    car: 'engine',
     info: "To add an item to an array, you can use a method called PUSH.",
     challenge: "Add an engine to your tracks by typing in the following code:",
-    answer: "myTracks.push(‘engine’);",
+    answer: "myTracks.push('engine');",
     action: () => {
-      myTracks.push("engine");
-      actionPush('engine');
+      myTracks.push('engine');
     },
   },
   {
     hint: false,
-    car: 'boxcar',
+    car: "boxcar",
     info: "Fantastic! Let’s add a boxcar. The push method always adds the item to the end of the array.",
     challenge:
       "Type the following code to add a boxcar to the end of your train:",
     answer: `myTracks.push('boxcar');`,
-    action: function() {
-      myTracks.push(this.car);
-      actionPush('boxcar');
-      console.log(this.car);
+    action: function () {
+      myTracks.push('boxcar');
     },
   },
   {
     hint: false,
     info: "Now let’s add a passenger car.",
-    challenge:
-      "Type the code to add a passenger car:",
-    answer: `myTracks.push('passenger car');`,
+    challenge: "Type the code to add a passenger car:",
+    answer: `myTracks.push('passenger-car');`,
     action: () => {
-      myTracks.push('passenger car');
-      actionPush('passenger car');
+      myTracks.push('passenger-car');
     },
   },
   {
     hint: true,
     info: "Challenge time!",
-    challenge:
-      "Can you add a caboose without looking at the hint?",
+    challenge: "Can you add a caboose without looking at the hint?",
     answer: `myTracks.push('caboose');`,
-    action: function() {
+    action: function () {
       myTracks.push('caboose');
-      actionPush('caboose');
     },
   },
   {
     hint: false,
     info: "Well done! Let’s add a tanker car.",
     challenge: "Type the code to add a tanker car:",
-    answer: `myTracks.push('tanker car');`,
-    action: function() {
-      myTracks.push('tanker car');
-      actionPush('tanker car');
+    answer: `myTracks.push('tanker-car');`,
+    action: function () {
+      myTracks.push('tanker-car');
     },
   },
   {
@@ -166,9 +186,8 @@ const levelInfo = [
     info: "Hm, the caboose should be on the end. Let’s use the POP method to pop the tanker off the end of the array.",
     challenge: "Type the following code to remove the last item in the array:",
     answer: `myTracks.pop();`,
-    action: function() {
+    action: function () {
       myTracks.pop();
-      actionPop();
     },
   },
   {
@@ -176,30 +195,26 @@ const levelInfo = [
     info: "Now do the same thing to the caboose.",
     challenge: "Type the code to remove the last item in the array:",
     answer: `myTracks.pop();`,
-    action: function() {
+    action: function () {
       myTracks.pop();
-      actionPop();
     },
   },
   {
     hint: true,
     info: "Let's add them back on in the correct order.",
     challenge: "Use the push method to add a tanker car.",
-    answer: `myTracks.push(‘tanker car’);`,
-    action: function() {
-      myTracks.push('tanker car');
-      actionPush('tanker car');
+    answer: `myTracks.push('tanker-car');`,
+    action: function () {
+      myTracks.push('tanker-car');
     },
   },
   {
     hint: true,
     info: "Perfect! Now we can reinstall the caboose.",
-    challenge:
-      "Use the push method to add a caboose.",
+    challenge: "Use the push method to add a caboose.",
     answer: `myTracks.push('caboose');`,
-    action: function() {
+    action: function () {
       myTracks.push('caboose');
-      actionPush('caboose');
     },
   },
   {
@@ -207,9 +222,8 @@ const levelInfo = [
     info: "Push and pop are great, but what if we want to remove an item at the other end of the array? For that, we can use the SHIFT method.",
     challenge: "Type the code to remove the first item in the array:",
     answer: `myTracks.shift();`,
-    action: function() {
+    action: function () {
       myTracks.shift();
-      actionShift();
     },
   },
   {
@@ -217,9 +231,8 @@ const levelInfo = [
     info: "Great! Now we can use the UNSHIFT method to add an item to the front.",
     challenge: "Type the code to add a flatcar to the front of the array;",
     answer: `myTracks.unshift('flatcar');`,
-    action: function() {
+    action: function () {
       myTracks.unshift('flatcar');
-      actionUnshift('flatcar');
     },
   },
   {
@@ -227,9 +240,8 @@ const levelInfo = [
     info: "Our train won’t get far without an engine!",
     challenge: "Type the code to add an engine to the front of the array:",
     answer: `myTracks.unshift('engine');`,
-    action: function() {
+    action: function () {
       myTracks.unshift('engine');
-      actionUnshift('engine');
     },
   },
   {
@@ -237,69 +249,22 @@ const levelInfo = [
     info: "Most array methods have a return value in addition to their primary action. SHIFT’s return value is the item it removed. This allows us to take an item from the front and put it somewhere else.",
     challenge: "Type the code to move the engine to the back of the array:",
     answer: `myTracks.push(myTracks.shift());`,
-    action: function() {
+    action: function () {
       myTracks.push(myTracks.shift());
-      actionShift();
-      actionPush('engine');
     },
   },
   {
     hint: false,
     info: "The POP method also returns the item it removes.",
-    challenge: "Type the code to remove the engine from the back and place it in the front.",
+    challenge:
+      "Type the code to remove the engine from the back and place it in the front.",
     answer: `myTracks.unshift(myTracks.pop());`,
-    action: function() {
+    action: function () {
       myTracks.unshift(myTracks.pop());
-      actionPop();
-      actionUnshift('engine');
     },
   },
-  
 ];
 
-function actionPush(src) {
-	const trainCar = document.createElement('img');
-  trainCar.src = `img/${src}.svg`;
-  trainCar.className = 'slot';
-  trainWrapper.appendChild(trainCar);
-}
-
-function actionPop() {
-  trainWrapper.removeChild(trainWrapper.lastElementChild);
-  }
-  
-function actionShift() {
-  trainWrapper.removeChild(trainWrapper.firstElementChild);
-  }
-
-function actionUnshift(src) {
-	const trainCar = document.createElement('img');
-  trainCar.src = `/img/${src}.svg`;
-  trainCar.className = 'slot';
-trainWrapper.insertBefore(trainCar, trainWrapper.firstChild);
-}
-
-function actionRemove(index) {
-  const childToRemove = trainWrapper.childNodes[index]; 
-  if (childToRemove) {
-    trainWrapper.removeChild(childToRemove);
-  } else {
-    console.log("There is no third child element.");
-  }
-}
-
-function actionAdd(src, index) {
-  alert('why????');
-  const newItem = document.createElement('img');
-  newItem.src = `/img/${src}.svg`;
-  trainWrapper.insertBefore(newItem, trainWrapper.children[index]);
-}
-
 function indexSelect(index) {
-	trainWrapper.children[index].style.boxShadow = 'inset 0 0 5px #303030';
+  trainWrapper.children[index].style.boxShadow = "inset 0 0 5px #303030";
 }
-
-  
-  
-
-
